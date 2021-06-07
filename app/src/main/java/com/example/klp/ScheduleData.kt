17ea441376144d801c928079cfd91cf5521753ca
hwkim98 <ksi05503@gmail.com>
2021-06-07 20:06:23 +0900

    schedule data class 추가

---
+++ app/src/main/java/com/example/klp/ScheduleData.kt
@@ -0,0 +1,12 @@
+package com.example.klp
+
+import java.io.Serializable
+import android.icu.util.*
+
+data class ScheduleData(var uid:String, var sid:Int, var sname:String, var sdate:String, var stime:String,
+                        var sregular:Int, var stype:String, var sestimate:Int, var simportance:Int, var sdetail:String) {
+
+    public fun getName():String{
+        return sname
+    }
+}
