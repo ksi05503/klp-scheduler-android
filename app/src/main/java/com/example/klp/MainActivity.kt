2b6c1bc4e617c4bfece7e40bbd900f1b101e75c5
hwkim98 <ksi05503@gmail.com>
2021-06-07 20:06:58 +0900

    Schedule View Model 추가 [ADD action 구현] (add schedule dialog)

--- app/src/main/java/com/example/klp/MainActivity.kt
+++ app/src/main/java/com/example/klp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.klp
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.widget.*
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.klp.databinding.ActivityMainBinding
+import androidx.lifecycle.Observer
+
 import com.google.android.material.tabs.TabLayoutMediator
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var scheduleViewModel: ScheduleViewModel  //add Schedule 뷰모델
+    
     lateinit var binding:ActivityMainBinding
     val fragArr = arrayListOf<String>("전체 목표", "오늘 실천", "나의 통계", "커뮤니티")
 
@@ -23,6 +36,93 @@ class MainActivity : AppCompatActivity() {
             tab, position ->
             tab.text = fragArr[position]
         }.attach()
+        dialogBuilder()
+    }
+
+
+    private fun dialogBuilder(){    //일정추가 dialog
+        binding.addBtn.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+
+            val dialogView = layoutInflater.inflate(R.layout.dialog_add_schedule,null)
+            val dialogScheduleName = dialogView.findViewById<EditText>(R.id.scheduleNameInputText)
+            val dialogScheduleTypeSpinner = dialogView.findViewById<Spinner>(R.id.spinner1)
+            val dialogRegularRadioGroup = dialogView.findViewById<RadioGroup>(R.id.regularRadiogroup)
+            val dialogEstimatedTimeRadioGroup = dialogView.findViewById<RadioGroup>(R.id.estimatedTimeRadiogroup)
+            val dialogImportanceRadioGroup = dialogView.findViewById<RadioGroup>(R.id.importanceRadiogroup)
+            val dialogDetail = dialogView.findViewById<EditText>(R.id.emptyTextField)
+
+            val calBtn = dialogView.findViewById<Button>(R.id.dateBtn)
+            val timeBtn = dialogView.findViewById<Button>(R.id.timeBtn)
+            var dbDate = ""  //db에 들어갈 string
+            var dbTime = ""  //db에 들어갈 string
+            calBtn.setOnClickListener {
+                var calendar = Calendar.getInstance()
+                var year = calendar.get(Calendar.YEAR)
+                var month = calendar.get(Calendar.MONTH)
+                var day = calendar.get(Calendar.DAY_OF_MONTH)
+
+                var date_listener = object : DatePickerDialog.OnDateSetListener{
+                    override fun onDateSet(
+                        view: DatePicker?,
+                        year: Int,
+                        month: Int,
+                        dayOfMonth: Int
+                    ) {
+                        calBtn.text = "$year/${month+1}/$dayOfMonth"
+                        dbDate = "${year.toString()}/${month.toString()}/${dayOfMonth}"
+                    }
+                }
+                var builder = DatePickerDialog(this, date_listener, year, month, day)
+                builder.show()
+            }
+
+            timeBtn.setOnClickListener {
+                var time = Calendar.getInstance()
+                var hour = time.get(Calendar.HOUR)
+                var minute = time.get(Calendar.MINUTE)
+
+                var timeListener = object: TimePickerDialog.OnTimeSetListener{
+                    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+                        timeBtn.text = "$hourOfDay : $minute"
+                        dbTime = "${hourOfDay.toString()}:${minute.toString()}"
+                    }
+                }
+                var builder = TimePickerDialog(this, timeListener, hour, minute, false)
+                builder.show()
+            }
+
+
+            builder.setView(dialogView)
+                .setPositiveButton("확인"){ dialogInterface, i ->
+
+                    scheduleViewModel = ViewModelProvider(this).get(ScheduleViewModel::class.java)
+
+
+                    binding.apply{
+                        val name = dialogScheduleName.text.toString()
+                        val type = dialogScheduleTypeSpinner.selectedItem.toString()
+                        val regular = dialogRegularRadioGroup.indexOfChild(dialogView.findViewById<RadioButton>(dialogRegularRadioGroup.checkedRadioButtonId))
+                        val estimate = dialogEstimatedTimeRadioGroup.indexOfChild(dialogView.findViewById<RadioButton>(dialogEstimatedTimeRadioGroup.checkedRadioButtonId))
+                        val importance = dialogImportanceRadioGroup.indexOfChild(dialogView.findViewById<RadioButton>(dialogImportanceRadioGroup.checkedRadioButtonId))
+                        val detail = dialogDetail.text.toString()
+//              입력된 DATA 정보들은 위와같다. (정규일정여부와 소요시간과 중요도(라디오버튼input)는 index정보로 db에 들어갈것이다)
+                        val newSchedule:ScheduleData = ScheduleData("임시id", -1, name,dbDate,dbTime,regular,type,estimate,importance,detail)
+                        scheduleViewModel.handleScheduleDB(actionType = ActionType.Add, newSchedule)
+                //뷰모델 데이터에 dialog input 스케줄 추가
+                        Toast.makeText(this@MainActivity, "뷰모델에 추가된 일정: ${scheduleViewModel.newSchedule.value?.getName()}",Toast.LENGTH_SHORT).show()
+
+                     }
+
+                }
+                .setNegativeButton("취소"){ dialogInterface, i ->
+                    /* 취소일 때 아무 액션이 없으므로 빈칸 */
+
+                }
+                .show()
+        }
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/klp/ScheduleViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.klp
+
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+enum class ActionType{
+    Add,Delete,Modify
+}
+class ScheduleViewModel: ViewModel() {
+
+    //내부에서 설정하는 자료형은 뮤터블로 변경가능하도록 설정
+
+    private val _newSchedule = MutableLiveData<ScheduleData>()
+
+    //변경되지 않는 데이터를 가저올때 이름을 _ 언더스코어 없이 설정
+    //공개적으로 가져오는 변수는 private이 아닌 퍼블릭으로 외부에서 접근가능하도록 설정
+    //값을 직접 라이브데이터에 접근하지 않고 뷰모델을 통해 가져올 수 있도록 설정
+
+    val newSchedule: LiveData<ScheduleData>
+        get() = _newSchedule        //get함수 대신 그냥 바로 newSchedule을 가져오면 된다
+
+    init{
+        //스케줄 초기화 필요하다면 추가
+    }
+
+    fun handleScheduleDB(actionType: ActionType, input: ScheduleData){
+        _newSchedule.value = input
+
+        when(actionType){
+/*            ActionType.Add ->
+*/
+
+            //db에 추가
+/*            ActionType.Delete ->
+                //db에서 삭제
+            ActionType.Modify ->
+                //db에 변경
+*/       }
+
+
+    }
+/*
+
+                    scheduleViewModel.newSchedule.observe(this, Observer {
+
+                    })  를 이용해서 값이 변경될때마다 목표 프래그먼트의 리사이클러뷰 아이템을 고쳐주면 될듯 하다.
+*/
+
+
+}
