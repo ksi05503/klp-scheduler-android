2b6ce1d7dcf64ebffa30287ca920d5873166800b
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-06-08 20:30:15 +0900

    커뮤니티 임시 XML 수정

--- app/src/main/java/com/example/klp/datas/Article.kt
+++ app/src/main/java/com/example/klp/datas/Article.kt
@@ -1,4 +1,4 @@
 package com.example.klp.datas
 
 //말머리, 글제목, 글쓴이 아이디, 성취도 점수, 좋아요 개수, 댓글 개수
-data class Article(var category:String, var Title:String, var author:String, var goalScore:Int, var prefNum:Int, var commentNum:Int)
+data class Article(var category:String, var Title:String, var author:String, var prefNum:Int, var commentNum:Int)
