2d81438f6a03645e5e844c449048bc96dc99fc1f
DongHyeon Park <paramore@kakao.com>
2021-06-20 00:28:03 +0900

    Community 작업 진행

--- app/src/main/java/com/example/klp/community/ArticleListAdapter.kt
+++ app/src/main/java/com/example/klp/community/ArticleListAdapter.kt
@@ -1,18 +1,25 @@
 package com.example.klp.community
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.klp.data.Article
+import com.example.klp.databinding.ArticleRowBinding
 
-class ArticleListAdapter(val scheList:List<Article>):
+class ArticleListAdapter(val articleList:ArrayList<Article>):
     RecyclerView.Adapter<ArticleListAdapter.ViewHolder>() {
-        inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
-
+        inner class ViewHolder(binding:ArticleRowBinding):RecyclerView.ViewHolder(binding.root){
+            val formHead = binding.formHeadText
+            val likes = binding.likesText
+            val commentNum = binding.commentNumText
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(ArticleRowBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -20,6 +27,6 @@ class ArticleListAdapter(val scheList:List<Article>):
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return articleList.size
     }
 }
